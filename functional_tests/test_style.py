7786ac1db4efacd92f4a2b7a64ba0ecc4a882ea0
Felix Chapman <aelred717@gmail.com>
2015-05-29 22:33:50 +0100

    Add basic bootstrap style css

---
+++ functional_tests/test_style.py
@@ -0,0 +1,27 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+
+class StyleTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_styling_applied(self):
+        self.browser.get(self.live_server_url)
+
+        # The user sizes their window
+        self.browser.set_window_size(1024, 768)
+
+        # The user sees the input box is centered
+        width = self.browser.get_window_size()['width']
+        inputbox = self.browser.find_element_by_id('search')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            width / 2,
+            delta=5
+        )
