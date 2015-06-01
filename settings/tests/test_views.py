82b60de9add8416f969c7c4272e4029d774bc5cc
Felix Chapman <aelred717@gmail.com>
2015-06-01 13:53:15 +0100

    Move settings tests into separate files

--- settings/tests/test_views.py
+++ settings/tests/test_views.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 
-from settings.config import config, write, read
 import rook.startup
 
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 
 @patch('settings.config.write')
@@ -60,32 +59,3 @@ class TestSettingsView(TestCase):
         self.assertFalse(write_func.called)
         self.client.post('/settings', data=data)
         self.assertTrue(write_func.called)
-
-
-@patch('builtins.open')
-class TestConfig(TestCase):
-
-    def tearDown(self):
-        # reset config file
-        rook.startup.run()
-
-    @patch('settings.config.config.read')
-    def test_read(self, read_func, open_func):
-        open_func.return_value = MagicMock()
-        enter = open_func.return_value.__enter__.return_value
-        self.assertFalse(read_func.called)
-        read()
-        read_func.assert_called_with(enter)
-
-    @patch('settings.config.config.write')
-    def test_write(self, write_func, open_func):
-        open_func.return_value = MagicMock()
-        enter = open_func.return_value.__enter__.return_value
-        self.assertFalse(write_func.called)
-        write()
-        write_func.assert_called_with(enter)
-
-    def test_defaults(self, open_func):
-        self.assertEqual(config['utorrent']['host'], 'localhost:8080')
-        self.assertEqual(config['utorrent']['username'], 'admin')
-        self.assertEqual(config['utorrent']['password'], '')
