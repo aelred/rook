880e0d2e3c88574f33c7a600203f67a675b05f04
Felix Chapman <aelred717@gmail.com>
2015-06-03 20:17:07 +0100

    Add logging to terminal

--- rook/settings.py
+++ rook/settings.py
@@ -96,3 +96,21 @@ TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
 # test runner
 TEST_RUNNER = 'rook.test_runner.TestRunner'
+
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        }
+    },
+}
