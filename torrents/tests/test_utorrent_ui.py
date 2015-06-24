49cfcfdb2dfa0160d27b146c82870c999318d821
Felix Chapman <aelred717@gmail.com>
2015-06-24 20:27:47 +0100

    Add way to check utorrent download progress

--- torrents/tests/test_utorrent_ui.py
+++ torrents/tests/test_utorrent_ui.py
@@ -5,7 +5,42 @@ from unittest.mock import patch, Mock
 import torrents.utorrent_ui
 
 
+UT_LIST = (
+    200,
+    {
+        'label': [['tv', 1]],
+        'torrentc': '739195965',
+        'torrents': [
+            [
+                '028ADCDD7A96F7F151BC0FB618E0777E2B2F69F9', 201,
+                'Firefly.2002.01x03.x264-BLOW', 5972552749, 1000,
+                5972552749, 3351314432, 561, 45974, 53, 210596, 'tv', 1, 3, 0,
+                0, 82552, -1, 0, '', '', 'Seeding 100.0 %', '1db11b69',
+                1426286134, 1426289247, '',
+                'C:\\downloads\\Firefly.2002.01x03.x264-BLOW', 0, 'C53FC3CE'
+            ],
+            [
+                'BA9FFD0202F6A101376D9163CA7FB9D474DEA8AC', 201,
+                '[ChoeyXD] One Piece 355 (Dual Audio) [278F9B0B].mkv',
+                430939486, 9, 3997696, 0, 0, 819, 481801, 23941076, 'video',
+                0, 2, 3, 3, 196608, 1, 426941790, '', '', 'Downloading 0.9 %',
+                'f', 1435173243, 0, '', 'D:\\Torrents', 0, '1E9F6125'
+            ]
+        ],
+        'rssfeeds': [],
+        'build': 31466,
+        'rssfilters': []
+    }
+)
+
+
 class TestUTorrent(TestCase):
+
+    def setUp(self):
+        self.down_1 = Mock()
+        self.down_1.utorrent_hash = '028ADCDD7A96F7F151BC0FB618E0777E2B2F69F9'
+        self.down_2 = Mock()
+        self.down_2.utorrent_hash = 'BA9FFD0202F6A101376D9163CA7FB9D474DEA8AC'
 
     @patch('torrents.utorrent_ui.ut_client')
     def test_download(self, ut_client):
@@ -21,3 +56,10 @@ class TestUTorrent(TestCase):
         client.assert_called_with('http://1.2.3.4:8/gui/', 'me', 'not telling',
                                   timeout=1)
         self.assertNotEqual(old_client, torrents.utorrent_ui.ut_client)
+
+    @patch('torrents.utorrent_ui.ut_client')
+    def test_get_completed(self, ut_client):
+        ut_client.list.return_value = UT_LIST
+
+        self.assertTrue(torrents.utorrent_ui.get_completed(self.down_1))
+        self.assertFalse(torrents.utorrent_ui.get_completed(self.down_2))
--- torrents/utorrent_ui.py
+++ torrents/utorrent_ui.py
@@ -12,3 +12,15 @@ def set_params(host, username, password):
 
 def download(download_obj):
     ut_client.addurl(download_obj.torrent.url)
+
+
+def _get_data(download_obj):
+    torrents = ut_client.list()[1]['torrents']
+    data = next(t for t in torrents if t[0] == download_obj.utorrent_hash)
+    return {
+        'percent': data[4]
+    }
+
+
+def get_completed(download_obj):
+    return _get_data(download_obj)['percent'] == 1000
